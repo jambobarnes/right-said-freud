7357efc40a1d5b18313c87eafaa0cfde1bf71e8b
James Barnes <james.barnes@approach-ps.com>
2022-01-13 00:59:25 +0000

    corrected meta title, added meta image

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,7 +1,7 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 
 class MyDocument extends Document {
-    static async getInitialProps(ctx:any) {
+    static async getInitialProps(ctx: any) {
         const initialProps = await Document.getInitialProps(ctx)
         return { ...initialProps }
     }
@@ -10,10 +10,11 @@ class MyDocument extends Document {
         return (
             <Html>
                 <Head>
-                <meta
-                    property="og:title"
-                    content="Rights Said Freud Podcast"
-                />
+                    <meta
+                        property="og:title"
+                        content="Right Said Freud Podcast"
+                    />
+                    <meta property="og:image" content="rsf.png" />
                     <link rel="preconnect" href="https://fonts.googleapis.com" />
                     <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
                     <link href="https://fonts.googleapis.com/css2?family=Arimo&amp;family=Bebas+Neue&amp;display=swap" rel="stylesheet" />
